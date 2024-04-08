11a58da7a7e8c38e4993652af9673b00fb7863e3
a4z <harald.achitz@gmail.com>
2024-04-08 19:05:49 +0200

    remove tmp files

--- cmake/gcc-13-vcpkg.cmake
+++
@@ -1,5 +0,0 @@
-
-get_filename_component(TRIPLET_DIR "${CMAKE_CURRENT_LIST_FILE}" DIRECTORY)
-include(${TRIPLET_DIR}/gcc-13-toolchain.cmake)
-include($ENV{HOME}/vcpkg/scripts/buildsystems/vcpkg.cmake)
-
