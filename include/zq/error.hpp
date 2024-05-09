cb9a50548c02c8b844ade4a13557c4787f3a0045
a4z <harald.achitz@gmail.com>
2024-05-09 09:46:58 +0200

    Remove strong type dependency

--- include/zq/error.hpp
+++ include/zq/error.hpp
@@ -1,20 +1,59 @@
 #pragma once
 
-#include <strong_type/strong_type.hpp>
-
+#include <fmt/core.h>
 #include "config.hpp"
 
 namespace zq {
   // TODO, if this is the only one that brings in strong_type, review the
   // dependency
-  using Error = strong::type<int,
-                             struct zq_error_tag,
-                             strong::ordered,
-                             strong::ordered_with<int>,
-                             strong::equality,
-                             strong::equality_with<int>,
-                             strong::boolean,
-                             strong::ostreamable>;
+  class Error {
+   public:
+    constexpr explicit Error(int value) : value_(value) {}
+
+    // Ordered
+    constexpr bool operator<(const Error& other) const {
+      return value_ < other.value_;
+    }
+    constexpr bool operator>(const Error& other) const {
+      return value_ > other.value_;
+    }
+    constexpr bool operator<=(const Error& other) const {
+      return value_ <= other.value_;
+    }
+    constexpr bool operator>=(const Error& other) const {
+      return value_ >= other.value_;
+    }
+    // Equality
+    constexpr bool operator==(const Error& other) const {
+      return value_ == other.value_;
+    }
+    constexpr bool operator!=(const Error& other) const {
+      return value_ != other.value_;
+    }
+    // Comparison with int
+    constexpr bool operator<(int other) const { return value_ < other; }
+    constexpr bool operator>(int other) const { return value_ > other; }
+    constexpr bool operator<=(int other) const { return value_ <= other; }
+    constexpr bool operator>=(int other) const { return value_ >= other; }
+    constexpr bool operator==(int other) const { return value_ == other; }
+    constexpr bool operator!=(int other) const { return value_ != other; }
+    // Boolean
+    constexpr explicit operator bool() const { return value_ != 0; }
+    // Ostreamable
+    friend std::ostream& operator<<(std::ostream& os, const Error& error) {
+      return os << error.value_;
+    }
+
+    constexpr int value() const { return value_; }
+
+   private:
+    int value_;
+  };
+
+  // backwards compatibility to old Error definition, might be removed one day
+  constexpr int value_of(const Error& error) {
+    return error.value();
+  }
 
   constexpr Error NoError{0};
 
@@ -34,11 +73,29 @@ namespace zq {
 }  // namespace zq
 
 template <>
+struct fmt::formatter<zq::Error> : public fmt::formatter<int> {
+  template <typename FormatContext>
+  constexpr auto format(const zq::Error& e,
+                        FormatContext& ctx) const -> decltype(ctx.out()) {
+    return fmt::formatter<int>::format(e.value(), ctx);
+  }
+};
+
+template <>
 struct fmt::formatter<zq::ErrMsg> {
   constexpr auto parse(fmt::format_parse_context& ctx) { return ctx.begin(); }
 
   auto format(const zq::ErrMsg& e, fmt::format_context& ctx) {
     return fmt::format_to(ctx.out(), "{}, {}", e.error, e.message);
+  }
+};
+
+template <>
+struct fmt::formatter<std::runtime_error> {
+  constexpr auto parse(fmt::format_parse_context& ctx) { return ctx.begin(); }
+
+  auto format(const std::runtime_error& e, fmt::format_context& ctx) {
+    return fmt::format_to(ctx.out(), "{}", e.what());
   }
 };
 
@@ -49,12 +106,3 @@ namespace zq {
   }
 
 }  // namespace zq
-
-template <>
-struct fmt::formatter<std::runtime_error> {
-  constexpr auto parse(fmt::format_parse_context& ctx) { return ctx.begin(); }
-
-  auto format(const std::runtime_error& e, fmt::format_context& ctx) {
-    return fmt::format_to(ctx.out(), "{}", e.what());
-  }
-};
